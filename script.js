13d43eb22ba0489bd981595f75797891254d8caa
DDIron <sourlemonmachine@gmail.com>
2023-12-31 16:04:03 +1300

    v0.04 media queries

--- script.js
+++ script.js
@@ -4,12 +4,18 @@ let nav_text = `
 <nav>
     <a href="overview.html"><h3>Luthwardron Wiki</h3></a>
     <a href=""><i class="fa-brands fa-discord"></i></a>
+    <a href="">・Hall of Contributions</a>
+    <a href="">・Plot Map</a>
+    <a href="">・Jots</a>
+    <a href="javascript:void(0);" onclick="toggleNav()" class="dropdown">
+        <i class="fa-solid fa-bars"></i>
+    </a>
 </nav>
 `;
 let footer_text = `
 <footer>
     <small>
-        wiki made by <a href="/pages/temp.html">@Glargoe</a> c:
+        wiki made by <a href="temp.html">@Glargoe</a> c:
     </small>
 </footer>
 `
@@ -20,8 +26,11 @@ nav.innerHTML = nav_text
 let footer = document.getElementById("footer");
 footer.innerHTML = footer_text
 
-// TEST UPLOAD FROM FILE INTO PAGE
-
-// let page_content_text = ``
-// let page_content = document.getElementsByClassName("content");
-// page_content.innerHTML = page_content_text
+// RESPONSIVE NAV
+function toggleNav() {
+    if (nav.className === "glargnav") {
+      nav.className += " transformnav";
+    } else {
+      nav.className = "glargnav";
+    }
+  }
